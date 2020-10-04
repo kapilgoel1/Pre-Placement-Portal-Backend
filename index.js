409580255e9e3065ae06e492b35cccd0670599a1
kapil-wq <59357022+kapil-wq@users.noreply.github.com>
2020-10-04 11:47:09 +0530

    updated cors options

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 const corsOptions = {
-  origin: 'http://127.0.0.1:3000',
+  origin: 'http://localhost:3000',
   credentials: true,
 };
 app.use(cors(corsOptions));
