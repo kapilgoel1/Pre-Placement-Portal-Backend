1b1c826dae9d425565fc27d9a55faae7411e665a
kapil-wq <59357022+kapil-wq@users.noreply.github.com>
2021-04-04 23:46:29 +0530

    fixing error

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: process.env.CLIENT_URL,
   credentials: true,
 };
 app.use(cors(corsOptions));
